ccaf4ba7e7b81473151dd8a25c4011e567bf3891
Berk Coşkun Mermer <mermerberkx@gmail.com>
2025-06-28 00:48:19 +0300

    java101 kapsamında

---
+++ basamaktplami/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main  {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz:");
+        int sayi = input.nextInt();
+        int toplam = 0;
+
+
+        while (sayi>0){
+            int basamak = sayi%10;
+            toplam += basamak;
+            sayi = sayi/10;
+
+        }
+
+        System.out.println("Sayının basamaklar toplamı:"+ toplam);
+    }
+}
