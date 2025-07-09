b4379f8ded73a72b332648b1ce4053b41aff8767
Berk Coşkun Mermer <mermerberkx@gmail.com>
2025-07-09 14:54:28 +0300

    java101 kapsamında

---
+++ maashesaplayici/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee emp = new Employee("Berk",150000.0,43,2018);
+        System.out.println(emp.toString());
+
+    }
+
+}
